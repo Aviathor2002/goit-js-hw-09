5758be07e89bb543a8ea32027586e6515330e2ed
Oleksandr Lytvyniuk <109354604+Aviathor2002@users.noreply.github.com>
2022-12-09 19:41:29 +0200

    task 2, 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,30 @@
-const background = document.querySelector('body')
-const start = document.querySelector(`[data-start]`)
-const stop = document.querySelector(`[data-stop]`)
+const background = document.querySelector('body');
+const start = document.querySelector(`[data-start]`);
+const stop = document.querySelector(`[data-stop]`);
 
+const DELAY_TO_NEW_COLOR = 1000;
+let timerColor = null
 
-console.log(start)
+console.log(start);
 
-start.addEventListener(`click`, bodyColor )
+start.addEventListener(`click`, OnBodyStart);
+stop.addEventListener('click', onBodyStop);
 
+function OnBodyStart() {
+  start.disabled = true;
 
+  timerColor = setInterval(() => {
+    background.style.background = getRandomHexColor();
+    console.log(`Now we change color`);
+  }, DELAY_TO_NEW_COLOR);
+}
 
-
-
-
-
-
-function bodyColor(){
-    const randColor = getRandomHexColor()
-    background.style.background = randColor
+function onBodyStop() {
+  clearInterval(timerColor);
+  console.log('Colors not change');
+  start.disabled = false;
 }
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
